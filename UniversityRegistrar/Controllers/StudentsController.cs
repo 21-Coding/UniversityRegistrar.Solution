f2aebe970bcb39d95b06bd87bb6a0e6fb1fbadbf
Drake Wilcox <drakewilcox@gmail.com>
2020-03-23 11:13:35 -0700

    add index, details, edit, delete and add course routes. Co-authored-by: Dom <ddyoumans@icloud.com> Co-authored-by: Matt <tyalor.matt@protonmail.com> Co-authored-by: Patrick Kille <59419677+PRKille@users.noreply.github.com>

--- UniversityRegistrar/Controllers/StudentsController.cs
+++ UniversityRegistrar/Controllers/StudentsController.cs
@@ -15,5 +15,97 @@ namespace UniversityRegistrar.Controllers
     {
       _db = db;
     }
+
+    public ActionResult Index()
+    {
+      return View(_db.Items.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "Course");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Student student, int CourseId)
+    {
+      _db.Students.Add(student);
+      if (CourseId !=0)
+      {
+        _db.Enrollment.Add(new Enrollment() { CourseId = CourseId, StudentId = student.StudentId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult Details(int id)
+    {
+      Student selectedStudent = _db.Students
+        .Include(student => student.Courses)
+        .ThenInclude(join => join.Course)
+        .FirstOrDefault(student => student.StudentId == id);
+      return View(selectedStudent);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Student selectedStudent = _db.Students.FirstOrDefault(students => students.StudentId == id);
+      ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "Name");
+      return View(selectedStudent);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Student student, int CourseId)
+    {
+      if (CourseId !=0)
+      {
+        _db.Enrollment.Add(new Enrollment() { CourseId = CourseId, StudentId = student.StudentId });
+      }
+      _db.Entry(student).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult AddCourse(int id)
+    {
+      Student selectedStudent = _db.Students.FirstOrDefault(students => students.StudentId == id);
+      ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "Name");
+      return View(selectedStudent);
+    }
+
+    [HttpPost]
+    public ActionResult AddCourse(Student student, int CourseId)
+    {
+      if (CourseId != 0)
+      {
+        _db.Enrollment.Add(new Enrollment() { CourseId = CourseId, StudentId = student.StudentId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      Student selectedStudent = _db.Students.FirstOrDefault(students => students.StudentId == id);
+      return View(selectedStudent);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Student selectedStudent = _db.Students.FirstOrDefault(students => students.StudentId == id);
+      _db.Students.Remove(selectedStudent);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult DeleteCourse(int joinId)
+    {
+      Enrollment joinEntry = _db.Enrollment.FirstOrDefault(entry => entry.EnrollmentId == joinId);
+      _db.Enrollment.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
