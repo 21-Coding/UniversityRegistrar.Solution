d86531258b1e18255cc6fa4c903215079c0143c0
Drake Wilcox <drakewilcox@gmail.com>
2020-03-23 10:38:31 -0700

    add index, details, edit and delete routes. Co-authored-by: Dom <ddyoumans@icloud.com> Co-authored-by: Matt <tyalor.matt@protonmail.com> Co-authored-by: Patrick Kille <59419677+PRKille@users.noreply.github.com>

--- UniversityRegistrar/Controllers/CoursesController.cs
+++ UniversityRegistrar/Controllers/CoursesController.cs
@@ -14,6 +14,59 @@ namespace UniversityRegistrar.Controllers
     {
       _db = db; 
     }
+    public ActionResult Index()
+    {
+      List<Course> model = _db.Courses.ToList();
+      return View(model);
+    }
+    public ActionResult Create()
+    {
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Course course)
+    {
+      _db.Courses.Add(course);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
+    public ActionResult Details(int id)
+    {
+      Course selectedCourse = _db.Courses
+        .Include(course => course.Students)
+        .ThenInclude(join => join.Student)
+        .FirstOrDefault(course => course.CourseId == id);
+      return View(selectedCourse);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Course selectedCourse = _db.Courses.FirstOrDefault(course => course.CourseId == id);
+      return View(selectedCourse);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Course course)
+    {
+      _db.Entry(course).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      Course selectedCourse = _db.Courses.FirstOrDefault(course => course.CourseId == id);
+      return View(selectedCourse);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Course selectedCourse = _db.Courses.FirstOrDefault(course => course.CourseId ==id);
+      _db.Courses.Remove(selectedCourse);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
